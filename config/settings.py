fe9081ab5d231d89d46a09f0aa26cdc0efee27f0
Sobir Bobojonov <121563197+BobojonovSobir0576@users.noreply.github.com>
2024-07-17 17:02:49 +0500

    123

--- config/settings.py
+++ config/settings.py
@@ -129,7 +129,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = "/static/"
 
-MEDIA_URL = "/media/"
+MEDIA_URL = "/var/www/media/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media", "/var/www/media")
 
