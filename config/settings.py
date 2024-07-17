369a5956e61e3ae9d4e003a7d2173f2c253edf20
Sobir Bobojonov <121563197+BobojonovSobir0576@users.noreply.github.com>
2024-07-17 14:36:43 +0500

    .

--- config/settings.py
+++ config/settings.py
@@ -138,6 +138,26 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = True
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+    'user-identifier',  # Add any custom headers here
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:8000",
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "http://localhost:5174",
+]
 
 
 REST_FRAMEWORK = {
